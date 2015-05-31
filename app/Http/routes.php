f3424afa1b43883d544dfb6e2a1d4b3afdf6ccb5
unknown <you@example.com>
2015-05-31 10:34:25 -0700

    api

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -10,6 +10,9 @@
 | and give it the controller to call when that URI is requested.
 |
 */
+use App\Location;
+use App\Education;
+use App\Module;
 
 Route::filter('auth', function()
 {
@@ -24,10 +27,25 @@ Route::group(array('before'=>'auth'), function() {
 	Route::resource('opleidingen', 'EducationController');
 });
 
-// Route::resource('gebruikers', 'UserController');
-// Route::resource('locaties', 'LocationController');
-// Route::resource('modules', 'ModuleController');
-// Route::resource('opleidingen', 'EducationController');
+Route::get('api/locaties', function(){
+	return Location::all();
+});
+
+Route::get('api/locaties/{locatieId}/opleidingen', function($locatieId){
+	$locations = Location::find($locatieId);
+	return $locations->educations;
+});
+
+Route::get('api/locaties/{locatieId}/opleidingen/{opleidingId}/modules', function($locatieId, $opleidingId){
+	$educations = Education::find($opleidingId);
+	return $educations->modules;
+});
+
+
+Route::get('api/locaties/{locatieId}/opleidingen/{opleidingId}/modules/{moduleId}', function($locatieId, $opleidingId, $moduleId){
+	return Module::where('id', '=', $moduleId)->get();
+});
+
 
 Route::get('/',['as' => 'home', 'uses' => 'HomeController@index']);
 
